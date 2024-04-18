c15e8bf6a507bdeed0eeaa164fc15d5ffb55c9a9
David López <davidlghellin@gmail.com>
2024-04-18 18:07:12 +0200

    Variables en fichero de configuración

--- infraestructure/src/run.rs
+++ infraestructure/src/run.rs
@@ -1,11 +1,21 @@
 use axum::Router;
+use dotenvy_macro::dotenv;
+use tracing::debug;
 
 use crate::create_routers::create_routes;
 
 pub async fn run() {
     let app: Router = create_routes();
+    let port: u16 = dotenv!("PORT").parse().unwrap();
+    let database: &str = dotenv!("DATABASE");
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    debug!("The database is: {}", database);
+    debug!("The port is:     {}", port);
+
+    let url: String = format!("{}:{}", database, port);
+    debug!("The url is:      {}", url);
+
+    axum::Server::bind(&url.parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
--- myapp/src/main.rs
+++ myapp/src/main.rs
@@ -1,24 +1,24 @@
+use dotenvy_macro::dotenv;
 use infraestructure::run::run;
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() {
-
     let subscriber: FmtSubscriber = FmtSubscriber::builder()
         // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
         // will be written to stdout.
         .with_max_level(Level::DEBUG) // TRACE
         // completes the builder.
         .finish();
+    let david_name = dotenv!("DAVID_VAR");
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     info!("Iniciamos la app");
     info!("Hello, world!");
-    let usuario = domain::user::User::new("David".to_owned());
+    let usuario = domain::user::User::new(david_name.to_owned());
     info!("{}", usuario);
-
 
     run().await
 }
