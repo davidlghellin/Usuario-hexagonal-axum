e9b8e5ad9452902f0e8f43cf03f322f821217988
David López <davidlghellin@gmail.com>
2024-04-20 13:47:18 +0200

    Config y utils en core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod users;
 pub mod error;
+pub mod config;
+pub mod utils;
---
+++ core/src/utils/security_service.rs
@@ -0,0 +1,19 @@
+use std::sync::Arc;
+
+use mockall::automock;
+
+use crate::error::ConduitResult;
+
+/// A security service for handling JWT authentication.
+pub type DynSecurityService = Arc<dyn SecurityService + Send + Sync>;
+
+#[automock]
+pub trait SecurityService {
+    fn hash_password(&self, raw_password: &str) -> ConduitResult<String>;
+
+    fn verify_password(
+        &self,
+        stored_password: &str,
+        attempted_password: String,
+    ) -> ConduitResult<bool>;
+}
